89b09355f6268f00bbb6ffd03f01738428111eed
stefan <stefan>
1999-10-11 11:33:41 +0000

    small fix to order of things

--- events.h
+++ events.h
@@ -1,10 +1,10 @@
 extern unsigned long cycles;
 
-static __inline__ do_cycles(unsigned long cycles_to_add)
+static __inline__ void do_cycles(unsigned long cycles_to_add)
 {
   cycles+=cycles_to_add;
+  memory.handle_event(cycles);
   if(cycles > 8012800) /* FIXME */
     cycles -= 8012800;
-  memory.handle_event(cycles);
 }
 
