76b3d6ea4fdbf659b793f7c449d7a377efd52d1d
stefan <stefan>
1999-10-16 10:25:26 +0000

    Changed to the generic version

--- maccess.h
+++ maccess.h
@@ -8,13 +8,16 @@
 
 static __inline__ uae_u32 do_get_mem_long(uae_u32 *a)
 {
+    uae_u8 *b = (uae_u8 *)a;
     
-    return *a;
+    return (*b << 24) | (*(b+1) << 16) | (*(b+2) << 8) | (*(b+3));
 }
 
 static __inline__ uae_u16 do_get_mem_word(uae_u16 *a)
 {
-    return *a;
+    uae_u8 *b = (uae_u8 *)a;
+    
+    return (*b << 8) | (*(b+1));
 }
 
 static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
@@ -24,12 +27,20 @@ static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
 
 static __inline__ void do_put_mem_long(uae_u32 *a, uae_u32 v)
 {
-    *a = v;
+    uae_u8 *b = (uae_u8 *)a;
+    
+    *b = v >> 24;
+    *(b+1) = v >> 16;    
+    *(b+2) = v >> 8;
+    *(b+3) = v;
 }
 
 static __inline__ void do_put_mem_word(uae_u16 *a, uae_u16 v)
 {
-    *a = v;
+    uae_u8 *b = (uae_u8 *)a;
+    
+    *b = v >> 8;
+    *(b+1) = v;
 }
 
 static __inline__ void do_put_mem_byte(uae_u8 *a, uae_u8 v)
